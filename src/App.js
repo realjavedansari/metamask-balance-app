facd771070d6121fb23f234723c398c256fef2a2
Javed Ansari <javedansari6542@gmail.com>
2024-08-24 17:15:23 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,32 +4,40 @@ import Web3 from 'web3';
 function App() {
   const [account, setAccount] = useState('');
   const [balance, setBalance] = useState('');
+  const [web3, setWeb3] = useState(null);
 
   useEffect(() => {
-    connectToMetaMask();
+    if (window.ethereum) {
+      const web3Instance = new Web3(window.ethereum);
+      setWeb3(web3Instance);
+      
+      window.ethereum.on('accountsChanged', handleAccountsChanged);
+      
+      // Check if already connected
+      web3Instance.eth.getAccounts().then(handleAccountsChanged);
+    }
+    
+    return () => {
+      if (window.ethereum) {
+        window.ethereum.removeListener('accountsChanged', handleAccountsChanged);
+      }
+    };
   }, []);
+
+  const handleAccountsChanged = async (accounts) => {
+    if (accounts.length > 0) {
+      setAccount(accounts[0]);
+      updateBalance(accounts[0]);
+    } else {
+      setAccount('');
+      setBalance('');
+    }
+  };
 
   const connectToMetaMask = async () => {
     if (typeof window.ethereum !== 'undefined') {
       try {
-        // Request account access
         await window.ethereum.request({ method: 'eth_requestAccounts' });
-        const web3 = new Web3(window.ethereum);
-        
-        // Get the current account
-        const accounts = await web3.eth.getAccounts();
-        setAccount(accounts[0]);
-        
-        // Get the balance
-        const balanceWei = await web3.eth.getBalance(accounts[0]);
-        const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
-        setBalance(balanceEth);
-        
-        // Listen for account changes
-        window.ethereum.on('accountsChanged', (newAccounts) => {
-          setAccount(newAccounts[0]);
-          updateBalance(web3, newAccounts[0]);
-        });
       } catch (error) {
         console.error("Error connecting to MetaMask", error);
       }
@@ -38,10 +46,17 @@ function App() {
     }
   };
 
-  const updateBalance = async (web3, account) => {
-    const balanceWei = await web3.eth.getBalance(account);
-    const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
-    setBalance(balanceEth);
+  const disconnectFromMetaMask = async () => {
+    setAccount('');
+    setBalance('');
+  };
+
+  const updateBalance = async (account) => {
+    if (web3 && account) {
+      const balanceWei = await web3.eth.getBalance(account);
+      const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
+      setBalance(balanceEth);
+    }
   };
 
   return (
@@ -51,6 +66,7 @@ function App() {
         <div>
           <p>Connected Account: {account}</p>
           <p>Balance: {balance} ETH</p>
+          <button onClick={disconnectFromMetaMask}>Disconnect</button>
         </div>
       ) : (
         <button onClick={connectToMetaMask}>Connect to MetaMask</button>
